b68cf0a7664839038bfa8c71659feda728c19ad8
terahje <terahjequinn@gmail.com>
2021-05-21 14:05:20 -0700

    initial set up, hello world introduction

---
+++ TripleX.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Well, well, well, Mr. Anderson we meet again.  You thought it was over, but I never truly left.  You knew that, didn't you Neo.";
+    std::cout << std::endl;
+    std::cout << "This time however, you lose.  I have lit a fuse if you will, that will soon reach every explosive currently connected.  The restart has begun....";
+    std::cout << std::endl;
+    std::cout << "...Neo, its Morpheus. The time has come once again.  Use your mind Neo, crack the code...you're our only hope..";
+    return 0;
+}
